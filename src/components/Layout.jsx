7d9987b85b1b49912613cdd918d7118d3547d68f
PRASHANT THAKUR <thakurprashantbpt@gmail.com>
2026-06-08 01:05:40 +0530

    Fixed map location

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -97,7 +97,7 @@ export default function Layout() {
         <div className="max-w-7xl mx-auto px-4 flex items-center justify-between gap-4">
           <div className="text-left">
             <a
-              href="https://www.google.com/maps/search/?api=1&query=Plot+No+-895M,+khata+No-62,+SUBHANPUR+KHEKRA+BAGHPAT+UTTAR+PRADESH+-+201102"
+              href="https://www.google.com/maps/place/Shri+Sai+Mill+Board+Industries/@28.8248656,77.2480893,17z/data=!4m14!1m7!3m6!1s0x390c55909258237f:0xed43c9db90121edb!2sShri+Sai+Mill+Board+Industries!8m2!3d28.8248803!4d77.2480068!16s%2Fg%2F11ytkp3016!3m5!1s0x390c55909258237f:0xed43c9db90121edb!8m2!3d28.8248803!4d77.2480068!16s%2Fg%2F11ytkp3016?entry=ttu&g_ep=EgoyMDI2MDYwMS4wIKXMDSoASAFQAw%3D%3D"
               target="_blank"
               rel="noopener noreferrer"
               className="leading-relaxed inline-block hover:text-amber-400 transition-colors"
@@ -341,7 +341,7 @@ export default function Layout() {
             <div>
               <h3 className="font-display font-semibold text-stone-100 mb-3 text-lg">Contact Us</h3>
               <a
-                href="https://www.google.com/maps/search/?api=1&query=Plot+No+-895M,+khata+No-62,+SUBHANPUR+KHEKRA+BAGHPAT+UTTAR+PRADESH+-+201102"
+                href="https://www.google.com/maps/place/Shri+Sai+Mill+Board+Industries/@28.8248656,77.2480893,17z/data=!4m14!1m7!3m6!1s0x390c55909258237f:0xed43c9db90121edb!2sShri+Sai+Mill+Board+Industries!8m2!3d28.8248803!4d77.2480068!16s%2Fg%2F11ytkp3016!3m5!1s0x390c55909258237f:0xed43c9db90121edb!8m2!3d28.8248803!4d77.2480068!16s%2Fg%2F11ytkp3016?entry=ttu&g_ep=EgoyMDI2MDYwMS4wIKXMDSoASAFQAw%3D%3D"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="text-sm leading-relaxed mb-3 text-stone-400 hover:text-amber-400 transition-colors block"
